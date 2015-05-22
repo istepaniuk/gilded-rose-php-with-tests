990c7948e609c143685fb3a99d98c586a93323d3
Peter Kofler <peter.kofler@code-cop.org>
2015-05-23 01:30:07 +0200

    more tests

--- test/gilded_rose_test.php
+++ test/gilded_rose_test.php
@@ -7,9 +7,7 @@ require_once 'item_builder.php';
 class GildedRoseTest extends PHPUnit_Framework_TestCase {
 
     // At the end of each day our system lowers both values for every item
-    /**
-     * @test
-     */
+    /** @test */
     function shouldDecreaseSellInOfOrdinaryItem() {
         $initialSellIn = 5;
         $this->create->ordinaryItem()->toSellIn($initialSellIn);
@@ -20,6 +18,48 @@ class GildedRoseTest extends PHPUnit_Framework_TestCase {
         $this->assertThatSellInIs(equalTo($initialSellIn - 1)); // variant (1b), specific
     }
 
+    /** @test */
+    function shouldDecreaseQualityOfOrdinaryItem() {
+        $this->create->ordinaryItem()->item();
+    
+        $this->updateQuality();
+    
+        $this->assertThatQualityIs($this->decreasedBy(1)); // variant (2), remove local variable
+    }
+    
+    // Once the sell by date has passed, Quality degrades twice as fast
+    /** @test */
+    function shouldDecreaseQualityOfExpiredOrdinaryItemTwiceAsFast() {
+        $initialQuality = 13;
+        $this->create->expired()->ordinaryItem()->ofQuality($initialQuality);
+    
+        $this->updateQuality();
+    
+        $this->assertThatQualityIs(equalTo($initialQuality - 2));
+    }
+    
+    // boundary
+    /** @test */
+    function shouldDecreaseQualityOfOrdinaryItemOnLastDayStillByOne() {
+        $initialQuality = 9;
+        $this->create->almostExpired()->ordinaryItem()->ofQuality($initialQuality);
+    
+        $this->updateQuality();
+    
+        $this->assertThatQualityIs(equalTo($initialQuality - 1));
+    }
+    
+    // boundary
+    /** @test */
+    function shouldDecreaseQualityOfOrdinaryItemOnSellDateAlreadyByTwo() {
+        $initialQuality = 8;
+        $this->create->justExpired()->ordinaryItem()->ofQuality($initialQuality);
+    
+        $this->updateQuality();
+    
+        $this->assertThatQualityIs(equalTo($initialQuality - 2));
+    }
+    
     // --- infrastructure
 
     /** @var Item */
@@ -48,7 +88,7 @@ class GildedRoseTest extends PHPUnit_Framework_TestCase {
     }
 
     protected function decreasedBy($number) {
-        return increasedBy(-$number);
+        return $this->increasedBy(-$number);
     }
 
     protected function negative() {
