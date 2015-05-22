30c41d2fe48bf016fa06e9d97a71dbc07da30f0b
Peter Kofler <peter.kofler@code-cop.org>
2015-05-23 01:36:25 +0200

    test that conjured items degrade in Quality twice as fast

---
+++ test/gilded_rose_new_feature_test.php
@@ -0,0 +1,68 @@
+<?php
+
+require_once 'gilded_rose_test.php';
+
+// 39 test cases + 6 new test cases = 45 test cases
+class GildedRoseNewFeatureTest extends GildedRoseTest {
+
+    // "Conjured" items degrade in Quality twice as fast as normal items
+    /** @test */
+    function shouldDecreaseQualityOfConjuredItemTwiceAsFast() {
+        $this->create->conjuredItem()->item();
+
+        $this->updateQuality();
+
+        $this->assertThatQualityIs($this->decreasedBy(2));
+    }
+
+    // Once the sell by date has passed, Quality degrades twice as fast
+    /** @test */
+    function shouldDecreaseQualityOfExpiredConjuredItemTwiceAsFast() {
+        $this->create->expired()->conjuredItem()->item();
+
+        $this->updateQuality();
+
+        $this->assertThatQualityIs($this->decreasedBy(2 * 2));
+    }
+
+    // boundary (The Quality of an item is never negative)
+    /** @test */
+    function shouldNotDecreaseQualityOfConjuredItemWithOneQualityBelowZero() {
+        $this->create->conjuredItem()->ofQuality(1);
+
+        $this->updateQuality();
+
+        $this->assertThatQualityIs(not($this->negative()));
+    }
+
+    // boundary
+    /** @test */
+    function shouldNotDecreaseQualityOfExpiredConjuredItemWithOneQualityBelowZero() {
+        $this->create->expired()->conjuredItem()->ofQuality(1);
+
+        $this->updateQuality();
+
+        $this->assertThatQualityIs(not($this->negative()));
+    }
+
+    // boundary
+    /** @test */
+    function shouldNotDecreaseQualityOfExpiredConjuredItemWithTwoQualityBelowZero() {
+        $this->create->expired()->conjuredItem()->ofQuality(2);
+
+        $this->updateQuality();
+
+        $this->assertThatQualityIs(not($this->negative()));
+    }
+
+    // boundary
+    /** @test */
+    function shouldNotDecreaseQualityOfExpiredConjuredItemWithThreeQualityBelowZero() {
+        $this->create->expired()->conjuredItem()->ofQuality(3);
+
+        $this->updateQuality();
+
+        $this->assertThatQualityIs(not($this->negative()));
+    }
+
+}
