61b1e8bbfa4bdcb448a97681b977cb44f533aa41
Peter Kofler <peter.kofler@code-cop.org>
2015-06-18 20:08:49 +0200

    add two more tests for item, thanks Corinna

--- test/item_test.php
+++ test/item_test.php
@@ -8,10 +8,24 @@ class ItemTest extends PHPUnit_Framework_TestCase {
     /**
      * @test
      */
+    function shouldHaveASellInValue() {
+        $this->assertClassHasAttribute("sell_in", "Item");
+    }
+
+    /**
+     * @test
+     */
+    function shouldHaveAQualityValue() {
+        $this->assertClassHasAttribute("quality", "Item");
+    }
+
+    /**
+     * @test
+     */
     function shouldDisplayWithNameAndValues() {
         $itemBuilder = new ItemBuilder(
-                function($item){
-                }
+            function($item){
+            }
         );
         $item = $itemBuilder->agedBrie()->withSellIn(3)->ofQuality(7);
         assertThat($item->__toString(), is("Aged Brie, 3, 7"));
